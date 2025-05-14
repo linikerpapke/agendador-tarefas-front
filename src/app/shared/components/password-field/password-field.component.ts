ea09c19c09fee61fb7085c75be274787a8170aed
Liniker <papke.liniker@gmail.com>
2025-05-13 23:01:36 -0300

    create form logic

--- src/app/shared/components/password-field/password-field.component.ts
+++ src/app/shared/components/password-field/password-field.component.ts
@@ -1,17 +1,21 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, signal } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'input-password-field',
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, ReactiveFormsModule],
   templateUrl: './password-field.component.html',
   styleUrl: './password-field.component.scss'
 })
 export class PasswordFieldComponent {
   hide = signal(true);
+
+  @Input({required: true}) control!: FormControl;
+
   clickEvent(event: MouseEvent) {
     this.hide.set(!this.hide());
     event.stopPropagation();
